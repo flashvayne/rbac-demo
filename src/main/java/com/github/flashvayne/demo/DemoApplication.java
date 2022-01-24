89dfa63333e86382d2c03f3d62c61a30817a3b3e
vayne <flashvayne@gmail.com>
2022-01-24 21:11:42 +0800

    yml config and starter

---
+++ src/main/java/com/github/flashvayne/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.github.flashvayne.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DemoApplication启动类
+ *
+ * @author flashvayne
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+}
